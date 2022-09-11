ab3132b959df1e6fbe8e33b085e9a0f9967f24fe
Ivan Bega <bega.ivan03@gmail.com>
2022-09-11 23:38:45 +0300

    finish lab2

--- lab2/lab2c/main.go
+++ lab2/lab2c/main.go
@@ -2,8 +2,30 @@ package main
 
 import "fmt"
 
-var x int = 100
-
 func main() {
-	fmt.Println(&x)
+	chiEnergy := [16]int{2, 6, 523, 12, 6000, 52, 7, 4, 33, 22, 43, 423, 102, 64, 42, 109}
+	result := start(chiEnergy)
+	fmt.Println(result)
+}
+func start(array [16]int) int {
+	c := make(chan int)
+	go compute(array, 0, len(array)-1, c)
+	return <-c
+}
+func compute(array [16]int, lo int, hi int, c chan int) {
+	if lo == hi {
+		c <- array[lo]
+		return
+	}
+	c1 := make(chan int)
+	c2 := make(chan int)
+	go compute(array, lo, (lo+hi)/2, c1)
+	go compute(array, (lo+hi)/2+1, hi, c2)
+	energy1 := <-c1
+	energy2 := <-c2
+	if energy1 > energy2 {
+		c <- energy1
+	} else {
+		c <- energy2
+	}
 }
